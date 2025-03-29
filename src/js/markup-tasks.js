d671ae1cfa54a8a96678b912e8eec7b1389efd6f
Marvin Nox <sinedtex@gmail.com>
2025-03-29 22:45:20 +0200

    add ids for notes

--- src/js/markup-tasks.js
+++ src/js/markup-tasks.js
@@ -1,9 +1,9 @@
 export function taskMarkup(notesArr) {
-    return notesArr.map(({ title, description }) =>
-        `<li class="task-list-item">
+  return notesArr.map(({ id, title, description }) =>
+    `<li class="task-list-item" id="${id}">
         <button class="task-list-item-btn">Delete</button>
         <h3>${title}</h3>
         <p>${description}</p>
       </li>`
-    ).join("");
+  ).join("");
 };
--- src/js/tasks.js
+++ src/js/tasks.js
@@ -7,14 +7,13 @@ import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
 export let userNotes = getFromLocal() || [];
-
 export function addNewNote(event) {
     event.preventDefault();
     const note = {
+        id: generateId(),
         title: refs.inputTitle.value.trim(),
         description: refs.inputDescr.value.trim(),
     }
-
     if (!note.title || !note.description) {
         iziToast.error({
             title: 'Error',
@@ -31,10 +30,18 @@ export function addNewNote(event) {
 
 export function deleteNote(event) {
     if (event.target.tagName === "BUTTON") {
-        const item = event.target.closest('li');
-        userNotes = userNotes.filter(element => !(element.title === item.children[1].textContent));
+        const itemToDel = event.target.closest('li');
+        userNotes = userNotes.filter(element => !(element.id === itemToDel.id));
         addToLocal(userNotes);
         renderNote(userNotes, taskMarkup);
     }
     return;
+};
+
+function generateId() {
+    return '4xxx-yxxx'.replace(/[xy]/g, function (c) {
+        const r = Math.random() * 16 | 0;
+        const v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
 };
